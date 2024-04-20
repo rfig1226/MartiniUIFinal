42f1f1dce4c0f957e30cd346aa14f965a693c056
rfig1226 <rosa.fig@outlook.com>
2024-04-20 13:54:18 -0400

    adding some light styling for recipe section

--- static/recipe.js
+++ static/recipe.js
@@ -7,8 +7,16 @@ function fetchRecipeData(recipe_id) {
     dataType: "json", // Ensure you're expecting a JSON response
     success: function (response) {
       // Update page content with the fetched data
-      var recipeData = response.data;
-      createSteps(recipeData);
+      var recipe_data = response.data;
+
+      $(".recipe-header").text(
+        `How to Make a ${
+          recipe_data.flavor_profile.charAt(0).toUpperCase() +
+          recipe_data.flavor_profile.slice(1).toLowerCase()
+        } Martini: The ${recipe_data.recipe_name}`
+      );
+
+      createSteps(recipe_data);
     },
     error: function (xhr, status, error) {
       console.error("Error fetching recipe data:", error);
@@ -23,9 +31,13 @@ function createSteps(recipe_data) {
   for (let step in recipe_steps) {
     console.log(step);
     let step_title = recipe_steps[step].step_title;
+    let step_video = recipe_steps[step].step_video;
     console.log(step_title);
     let step_element = $(`<div class="step-item" id="${step}">
             <div class="step-title">${step_title}</div>
+            <div class="step-video-container">
+                <div class="step-video">${step_video}</div>
+            </div>
             <div class="sub-steps" id="sub-${step}"></div>
             </div>`);
 
@@ -34,7 +46,10 @@ function createSteps(recipe_data) {
 
     for (let sub_step in sub_steps) {
       let sub_step_element = $(
-        `<div class="sub-step-item">${sub_steps[sub_step]}</div>`
+        `<div class="sub-step-item">
+            <div class="sub-step-num">${parseInt(sub_step) + 1}.</div> 
+            <div class="sub-step-description">${sub_steps[sub_step]}</div>
+        </div>`
       );
       console.log(sub_step_element);
       $(`#sub-${step}`).append(sub_step_element);
@@ -43,6 +58,10 @@ function createSteps(recipe_data) {
     if (step === "step1") {
       step_element.addClass("active");
     }
+  }
+
+  if ($(".step-item.active").is(":first-child")) {
+    $("#prev-step-btn").prop("disabled", true);
   }
 }
 
@@ -56,6 +75,19 @@ $(document).ready(function () {
     if (nextStep.length) {
       currentActive.removeClass("active");
       nextStep.addClass("active");
+      // Always enable the 'Previous Step' button when moving from the first step
+      $("#prev-step-btn").prop("disabled", false);
+    }
+
+    // Automatically append the button if it's the last step
+    if (!nextStep.next(".step-item").length) {
+      $(this).text("Test Your Knowledge"); // Change the button text to "Go to Quiz"
+      $(this)
+        .off("click")
+        .click(function () {
+          // Remove previous click handlers and add new one
+          window.location.href = "/recipe_quiz/" + recipe_id; // Redirect to the quiz page
+        });
     }
   });
 
@@ -66,6 +98,17 @@ $(document).ready(function () {
     if (prevStep.length) {
       currentActive.removeClass("active");
       prevStep.addClass("active");
+      $("#next-step-btn").prop("disabled", false);
+    }
+
+    if (prevStep.is(":first-child")) {
+      $("#prev-step-btn").prop("disabled", true);
+    }
+
+    // Remove the quiz button if not on the last step anymore
+    if (!prevStep.is(":last-child")) {
+      // Check if it's no longer the last step
+      $("#go-recipe-quiz-btn").remove();
     }
   });
 });
