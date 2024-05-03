4bb6eade8b18d1ca4751e5ffeb3f050775a699e9
rfig1226 <rosa.fig@outlook.com>
2024-05-03 16:57:23 -0400

    added some styling to home page

--- static/start.js
+++ static/start.js
@@ -1,24 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $(".flavor-button").click(function () {
+    let flavor = $(this).data("flavor").toLowerCase();
+    let recipe_num;
+    console.log(lessons);
+    // Iterate over the lessons data to find the matching flavor profile
+    for (let recipe_id in lessons) {
+      console.log(recipe_id);
+      if (lessons.hasOwnProperty(recipe_id)) {
+        let recipe_data = lessons[recipe_id];
+        console.log("clicked");
+        console.log(flavor);
 
-$(".flavor-button").click(function() {
-        let flavor = $(this).data("flavor").toLowerCase();
-        let recipe_num;
-
-        // Iterate over the lessons data to find the matching flavor profile
-        for (let recipe_id in lessons) {
-            if (lessons.hasOwnProperty(recipe_id)) {
-                let recipe_data = lessons[recipe_id];
-                if (recipe_data['flavor_profile'] === flavor) {
-                    recipe_num = recipe_id;
-                    break;
-                }
-            }
+        if (recipe_data["flavor_profile"] === flavor) {
+          console.log("pulled:");
+          console.log(recipe_data["flavor_profile"]);
+          recipe_num = recipe_id;
+          break;
         }
-        // Redirect user to recipe page with the selected recipe number
-        if (recipe_num) {
-            window.location.href = "/start/" + recipe_num;
-        } else {
-            console.error("Recipe not found for flavor profile: " + flavor);
-        }
-    });
+      }
+    }
+    // Redirect user to recipe page with the selected recipe number
+    if (recipe_num) {
+      window.location.href = "/start/" + recipe_num;
+    } else {
+      console.error("Recipe not found for flavor profile: " + flavor);
+    }
+  });
 });
