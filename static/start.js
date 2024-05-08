483488f5305df03d45f24c54c9d74bcbe720105d
natattack03 <nb3044@columbia.edu>
2024-05-08 15:51:57 -0400

    dropdown menu and hover tooltip for start page

--- static/start.js
+++ static/start.js
@@ -3,27 +3,33 @@ $(document).ready(function () {
     let recipe_data = lessons[recipe_id];
     let image = recipe_data["images"][0];
     let profile = recipe_data["flavor_profile"];
+    let shortDescription = recipe_data["short_descript"]; // Fetch short description
     profile = profile[0].toUpperCase() + profile.slice(1);
+
     let flavor_column = $(
       `<div class="col-md-4 flavor-profile-column">
-        <div class="flavor-profile-img">
-            <img src=${image} alt="${profile} Martini Image" data-recipe-id="${recipe_id}" class="clickable">
-        </div> 
+        <div class="flavor-profile-container">
+            <img src=${image} alt="${profile} Martini Image" data-recipe-id="${recipe_id}" class="clickable flavor-image">
+            <div class="flavor-overlay clickable" data-recipe-id="${recipe_id}">
+              <div class="flavor-text">${shortDescription}</div>
+            </div>
+        </div>
         <div class="flavor-profile-title clickable" data-recipe-id="${recipe_id}">
             ${profile} Martini
         </div>
        </div>`
     );
 
+
+
     $(".flavor-profiles").append(flavor_column);
   }
 
   $(".flavor-profiles").on(
     "click",
-    ".flavor-profile-img img, .flavor-profile-title",
+    ".flavor-profile-container img, .flavor-profile-title, .flavor-overlay",
     function () {
       let recipe_id = $(this).data("recipe-id");
-      console.log(recipe_id);
       if (recipe_id || recipe_id === 0) {
         window.location.href = "/start/" + recipe_id;
       } else {
